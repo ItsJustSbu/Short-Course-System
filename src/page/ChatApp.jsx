2c5ac470f752f760e47896fc22672e022cb87142
Sibusiso Mfana <2307294@students.wits.ac.za>
2023-04-24 01:06:13 +0200

    setting up chat

--- src/page/ChatApp.jsx
+++ src/page/ChatApp.jsx
@@ -1,67 +1,25 @@
-import { useState, useEffect } from "react";
-import axios  from "axios";
+import SplitscreenLayout from "../layouts/SplitScreenLayout";
+import { Input } from "@material-tailwind/react";
+import SendIcon from "@mui/icons-material/Send"
 
 function ChatApp(){
 
-    const [messages, setMessages] = useState([]);
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-
-    useEffect(()=>{
-            (async () =>{
-                const response = axios.get('http://localhost:3000/message');
-                setMessages((await response).data);
-            })()
-            
-            
-        },[]);
-
-        async function handleSubmit(event){
-            event.preventDefault();
-            
-            try{
-                await axios.post('http://localhost:3000/message',{username, message});
-
-                setUsername('');
-                setMessage('');
-
-                const response = await axios.get('http://localhost:3000/message');
-                setMessages(response);
-            }catch(e){
-                console.log(e)
-            }
-        }
-
-        return(
-            <div>
-      <h1>Chat App</h1>
-      <ul>
-        {messages.map((message) => (
-          <li key={message._id}>
-            {message.username}: {message.message}
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Message"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
+  return(
+    <div className="w-full h-full">
+      <SplitscreenLayout>
+    <h1>This is the first part</h1>
+    <div className="flex w-full h-[50vh] bg-gray-50">
+      <div className="justify-center w-5/6">
+        <Input label=" Input with Icon" icon={<SendIcon />} />
+      </div>
     </div>
-        );
+  </SplitscreenLayout>
 
-    }
+    </div>
+    
+  )
+  
 
-
+}
 
 export default ChatApp;
