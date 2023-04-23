6796a028e695d1ecbee1cd94bd7e88b565245497
DarkAzureII <126709301+DarkAzureII@users.noreply.github.com>
2023-04-24 01:26:01 +0200

    have no words

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import HomePage from './page/HomePage';
 import RegistrationPage from './page/RegistrationPage';
 import LoginPage from './page/LoginPage';
+import VideoPage from './page/VideoPage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegistrationPage />}/>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/watch" element={<VideoPage />} />
       
     </Routes>
     </>
