82cb4fa8cbddf9f36e0adedc8da2a3ff1b33d07a
clam2001 <108890680+clam2001@users.noreply.github.com>
2023-05-18 16:38:00 +0200

    Update CourseInfoPage.jsx

--- frontend/src/page/CourseInfoPage.jsx
+++ frontend/src/page/CourseInfoPage.jsx
@@ -3,11 +3,12 @@ import Home from "../Home";
 import CourseInfoHeading from "../components/CourseInfoHeading";
 import CourseScroller from "../components/CourseScroller";
 
-
+//Return the course description page
 function CourseInfoPage() {
   const {state} = useLocation();
   console.log(state);
 
+  //navabar + courseinfo heading + course scroller
   return (
     <div>
       <Home></Home>
