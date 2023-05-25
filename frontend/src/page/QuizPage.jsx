9881dc7dce8e69e4e41e681c863265147e42b739
clam2001 <108890680+clam2001@users.noreply.github.com>
2023-05-25 23:09:06 +0200

    Create QuizPage.jsx

---
+++ frontend/src/page/QuizPage.jsx
@@ -0,0 +1,15 @@
+import Home from "../Home"
+import Questions from "../components/Questions"
+
+
+function QuizPage() {
+
+  return (
+      <div>
+          <Home></Home>
+          <Questions></Questions>
+    </div>
+  )
+}
+
+export default QuizPage;
